83dce22146666e40819bc2c82a70648c4b5126ee
taojian123 <1711854114@qq.com>
2019-05-14 15:07:14 +0800

    taojian

--- Day87SSMDubboweb/src/main/java/com/qf/controller/DepartmentController.java
+++ Day87SSMDubboweb/src/main/java/com/qf/controller/DepartmentController.java
@@ -1,25 +1,28 @@
 package com.qf.controller;
 
+import com.qf.pojo.DepartmentsBig;
 import com.qf.pojo.DepartmentsBigExample;
 import com.qf.service.IDepartmentsBigService;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
-@Controller
+@RestController
 public class DepartmentController {
 
-    @Resource
+    /*@Resource
     private IDepartmentsBigService departmentsBigService;
 
     @RequestMapping("/getAllDepart")
-    public Object getAll(){
+    public List<DepartmentsBig> getAll(){
 
         DepartmentsBigExample departmentsBigExample=new DepartmentsBigExample();
 
         return departmentsBigService.getAllDepartmentsBigByExample(departmentsBigExample);
-    }
+    }*/
 
 }
